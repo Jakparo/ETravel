08b5ae6a231ed5585fdc8a65d823a91e2fff685a
Jackparo <zingme321@gmail.com>
2020-07-04 21:44:37 +0700

    Connect MongoDB

--- server/server.js
+++ server/server.js
@@ -1,10 +1,29 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
 import data from './data';
+import config from './config';
+import userRoute from './routes/userRoute';
+
+dotenv.config();
+
 const app = express();
+const mongodbUrl = config.MONGODB_URL;
+
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(err => console.log( err.reason ));
+
+
+app.use("/api/users", userRoute);
+
 
 app.get("/api/products/:id",(req, res)=>{
     const productId = req.params.id;
-    const product = data.products.find(x=>x._id === productId); 
+    const product = data.products.find(x => x._id === productId); 
     if(product)
         res.send(product);
     else
